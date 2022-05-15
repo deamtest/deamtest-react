a3f731096ec9e480b6bb52e04cc064a264e089a3
DeamChain CEO <103885931+leopawel@users.noreply.github.com>
2022-05-15 05:48:46 +0100

    fix: lib conflict

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,10 @@
-import { FC } from 'react';
-
+/// <reference types="react" />
 interface DeamtestProps {
     lang?: string;
     appId: number;
     apiKey: string;
     onVerify: (token: string) => void;
 }
-declare const Deamtest: FC<DeamtestProps>;
+declare const Deamtest: ({ lang, appId, apiKey, onVerify }: DeamtestProps) => JSX.Element;
 
 export { Deamtest as default };
